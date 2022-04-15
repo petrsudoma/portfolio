06d90f02be896b77d5a009b7442c1b4bdf727773
Petr Šudoma <petrsudoma28@gmail.com>
2022-04-15 17:05:30 +0200

    setup configuration
    - prettier config
    - styled-components setup
    - global style setup
    - google font connected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-size: 20px;
+	font-family: 'Nunito', sans-serif;
+	font-weight: 400;
+	color: #fff;
+	background-color: #000;
+}`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+      <ThemeProvider theme={{}}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
