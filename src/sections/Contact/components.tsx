d4046d52a1f6df86015d4feb2413d306bce8d35e
Petr Šudoma <petrsudoma28@gmail.com>
2022-05-21 02:54:01 +0200

    frontend:
    - social link box

--- src/sections/Contact/components.tsx
+++ src/sections/Contact/components.tsx
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import {
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+  AiOutlineSolution,
+} from 'react-icons/ai';
 
 export const Flexbox = styled.div`
   display: flex;
@@ -7,8 +12,50 @@ export const Flexbox = styled.div`
   margin-bottom: 100px;
 `;
 
-export const LinkBox = styled.div`
+export const SocialBox = styled.div`
   width: 350px;
   height: 450px;
   box-shadow: 0 12px 56px 9px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CrownIcon = styled.svg`
+  width: 120px;
+  height: 120px;
+  transform: scale(0.7);
+  background-image: url('/crown-icon.svg');
+  margin-top: 5%;
+`;
+
+export const SocialLinkWrapper = styled.div`
+  width: 250px;
+  margin-top: 10%;
+`;
+
+export const GitHubIcon = styled(AiOutlineGithub)`
+  font-size: 2em;
+  float: left;
+  margin-right: 10%;
+`;
+
+export const LinkedinIcon = styled(AiOutlineLinkedin)`
+  font-size: 2em;
+  float: left;
+  margin-right: 10%;
+`;
+
+export const CVIcon = styled(AiOutlineSolution)`
+  font-size: 2em;
+  float: left;
+  margin-right: 10%;
+`;
+
+export const SocialLinkText = styled.p`
+  float: left;
+  margin-top: 7px;
+  font-weight: 200;
+  font-size: 0.9em;
+  color: ${(props) => props.theme.colors.gold};
 `;
--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Flexbox, LinkBox } from './components';
+import {
+  Flexbox,
+  SocialBox,
+  CrownIcon,
+  SocialLinkWrapper,
+  GitHubIcon,
+  SocialLinkText,
+  LinkedinIcon,
+  CVIcon,
+} from './components';
 
 const Contact = () => {
   return (
     <Flexbox>
-      <LinkBox>test</LinkBox>
+      <SocialBox>
+        <CrownIcon />
+        <SocialLinkWrapper>
+          <GitHubIcon fill='#fae100' />
+          <SocialLinkText>Checkout my code</SocialLinkText>
+        </SocialLinkWrapper>
+
+        <SocialLinkWrapper>
+          <LinkedinIcon fill='#fae100' />
+          <SocialLinkText>Look at my profile</SocialLinkText>
+        </SocialLinkWrapper>
+
+        <SocialLinkWrapper>
+          <CVIcon fill='#fae100' />
+          <SocialLinkText>Get my CV</SocialLinkText>
+        </SocialLinkWrapper>
+      </SocialBox>
     </Flexbox>
   );
 };
