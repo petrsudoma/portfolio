76c4c0418cdc8037c13cdc683bbf7011a11e0b46
Petr Šudoma <petrsudoma28@gmail.com>
2022-05-24 22:36:47 +0200

    backend:
    - cors policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { router as messageRouter } from './routes/message';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+	cors({
+		origin: [
+			'http://localhost:3000',
+			'https://petrsudoma-dev.vercel.app',
+			'https://petrsudoma.vercel.app/',
+		],
+	})
+);
 
 app.use(messageRouter);
 
