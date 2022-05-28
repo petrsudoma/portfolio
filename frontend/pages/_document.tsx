40856c18b1047fa25a256339c5c5cce0fdad7dc3
Petr Šudoma <petrsudoma28@gmail.com>
2022-05-28 12:12:45 +0200

    frontend:
    - security policy

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -14,10 +14,6 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Nunito:wght@200;400&display=swap'
           rel='stylesheet'
         />
-        <meta
-          httpEquiv='Content-Security-Policy'
-          content='upgrade-insecure-requests'
-        />
       </Head>
       <body>
         <Main />
